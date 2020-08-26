e9fc53072a5844d46829fddb2bf157eee7f5aeaf
rocker3011 <rockavenger3011@gmail.com>
2020-08-26 15:40:05 -0300

    react #1

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function HelloWorld(props: any) {
+  return <h1> Hello, {props.name} </h1>
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <HelloWorld name="World" />
+          <HelloWorld name="RODO" />
         </p>
         <a
           className="App-link"
